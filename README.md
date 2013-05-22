b4d1de004d2ff4cf89aaa251f58cf51515db2254
Don Walter <don@don-walter.com>
2013-05-21 20:31:15 -0400

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-jquery-mobile-slide-menu
-========================
+jQuery Mobile Slide Menu
+------------------------
+This is a simple side aligned sliding menu for jQuery Mobile. You can access the menu either by hitting the menu icon or by swiping your finger from left to right, to slide the menu out. Likewise, you can hide it with the menu icon, or by swiping your finger from right to left to slide the menu back in.
+
+Usage
+-----
+Include the following files:
+```html
+<link rel="stylesheet" href="css/jquery-mobile-slide-menu.css" type="text/css" />
+<script type="text/javascript" src="js/jquery-mobile-slide-menu.js"></script>
+```
+
+Then insert your menu, before your `<div data-role="page">` tag:
+```html
+<div id="side-menu">
+	<ul>
+		<li><a href="/">Main</a><span class="icon"></span></li>
+		<li><a href="/page-2/">Page 2</a><span class="icon"></span></li>
+		<li><a href="/page-3/">Page 3</a><span class="icon"></span></li>
+		<li><a href="/page-4/">Page 4</a><span class="icon"></span></li>
+	</ul>
+</div>
+```
+
+Then add this to initiate the menu:
+```javascript
+$(function(){
+	$('#side-menu').slideMenu();
+});
+```
+
+Demo
+----
+Demo in index.html
+
+License
+-------
+Copyright (c) 2013 Don Walter. Licensed under the MIT license.
---
+++ js/jquery-mobile-slide-menu.js
@@ -0,0 +1,71 @@
+/*
+ * jQuery Mobile Slide Menu 1.0.0, jQuery Mobile plugin
+ * https://github.com/donwalter/jquery-mobile-slide-menu
+ *
+ * Copyright(c) 2013, Don Walter
+ * http://www.don-walter.com/
+ *
+ * A side aligned sliding menu for jQuery Mobile
+ * Licensed under the MIT License
+ */
+
+(function($){
+	$.fn.slideMenu = function(options) {  
+		// If options exist, merge them with the default settings
+		options = $.extend({
+			duration:	500,
+			easing:		'swing'
+		}, options);
+		
+		return this.each(function() {
+			var obj = $(this);
+			
+			var menuStatus = false;
+		
+			$(document).on('click', 'a.showMenu', function(e) {
+				if(!menuStatus){
+					$('#side-menu').css('visibility','visible');
+					$('.ui-page-active').animate({
+						marginLeft: '165px',
+					}, options.duration, options.easing, function(){menuStatus = true});
+					return false;
+				} else {
+					$('.ui-page-active').animate({
+						marginLeft: '0px',
+					}, options.duration, options.easing, function(){menuStatus = false});
+					return false;
+				}
+			});
+		
+			$(document).on('swipeleft', '.pages', function(e) {
+				if (menuStatus){
+					$('.ui-page-active').animate({
+						marginLeft: '0px',
+					}, options.duration, options.easing, function(){menuStatus = false; $('#side-menu').css('marginTop', $(window).scrollTop());});
+				}
+			});
+		
+			$(document).on('swiperight', '.pages', function(e) {
+				if (!menuStatus){
+					//alert($(window).scrollTop() + ' - ' + $('#side-menu').offset().top);
+					$('#side-menu').css('marginTop', $(window).scrollTop());
+					$('#side-menu').css('visibility','visible');
+					$('.ui-page-active').animate({
+						marginLeft: '165px',
+					}, options.duration, options.easing, function(){menuStatus = true});
+				}
+			});
+		
+			$('#side-menu li a').click(function(){
+				var p = $(this).parent();
+				if($(p).hasClass('active')){
+					$('#side-menu li').removeClass('active');
+				} else {
+					$('#side-menu li').removeClass('active');
+					$(p).addClass('active');
+				}
+				menuStatus = false;
+			});
+		});
+	};
+})(jQuery);
